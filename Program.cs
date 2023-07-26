18cf77469fa5ffbab1ec87981455e76d4addd7c1
jbao8899 <jbao8899@gmail.com>
2023-07-26 17:43:58 -0600

    Created project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+// Go to package management console -> dotnet watch run -> run web app, update whenever
+// changes are made to code
+
+namespace entity_framework_tutorial_web_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
